ee4bceb4d52afe968174517b15d71e95154792e4
Huỳnh Hà <huynhsamha@gmail.com>
2020-03-08 08:26:30 +0700

    add build

---
+++ precache-manifest.21ee17f9d749ffc6698aca7bed23c725.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfbafd26525c322020b6adcbf97b085",
+    "url": "/datta-able-react-admin/index.html"
+  },
+  {
+    "revision": "471ed0d5ae79f7a5856b",
+    "url": "/datta-able-react-admin/static/css/2.b7858545.chunk.css"
+  },
+  {
+    "revision": "b4efaa40ecf9cf5d086a",
+    "url": "/datta-able-react-admin/static/css/5.11c35eea.chunk.css"
+  },
+  {
+    "revision": "0521d9eeeb1eb6eaa1fd",
+    "url": "/datta-able-react-admin/static/css/6.1950771a.chunk.css"
+  },
+  {
+    "revision": "8ac31503f423f06d5437",
+    "url": "/datta-able-react-admin/static/css/8.ecfc7541.chunk.css"
+  },
+  {
+    "revision": "5a33c6363a1056e8d83a",
+    "url": "/datta-able-react-admin/static/js/0.0172c614.chunk.js"
+  },
+  {
+    "revision": "c5955ef0cc5f31551345",
+    "url": "/datta-able-react-admin/static/js/1.f658fc8f.chunk.js"
+  },
+  {
+    "revision": "ddb2ec168ea78f726b21",
+    "url": "/datta-able-react-admin/static/js/10.65b1caae.chunk.js"
+  },
+  {
+    "revision": "4d3cf462a22527ab1097",
+    "url": "/datta-able-react-admin/static/js/11.b5a32adb.chunk.js"
+  },
+  {
+    "revision": "20df84eb570bd2032395",
+    "url": "/datta-able-react-admin/static/js/12.45d0a0c2.chunk.js"
+  },
+  {
+    "revision": "d594b960f572418a92ae",
+    "url": "/datta-able-react-admin/static/js/13.4e85bf3d.chunk.js"
+  },
+  {
+    "revision": "9104c8fbb230e4eaed32",
+    "url": "/datta-able-react-admin/static/js/14.76a9bec5.chunk.js"
+  },
+  {
+    "revision": "9071b8f15df3e97740ef",
+    "url": "/datta-able-react-admin/static/js/15.d46ad676.chunk.js"
+  },
+  {
+    "revision": "89d277538efcb047d30e",
+    "url": "/datta-able-react-admin/static/js/16.c90708bc.chunk.js"
+  },
+  {
+    "revision": "5fa024d5cea448ca2480",
+    "url": "/datta-able-react-admin/static/js/17.6b98a39f.chunk.js"
+  },
+  {
+    "revision": "52c73a331362701f5bc0",
+    "url": "/datta-able-react-admin/static/js/18.56b81543.chunk.js"
+  },
+  {
+    "revision": "31aa433227e56cf9b29d",
+    "url": "/datta-able-react-admin/static/js/19.268874b4.chunk.js"
+  },
+  {
+    "revision": "471ed0d5ae79f7a5856b",
+    "url": "/datta-able-react-admin/static/js/2.98a9a0b8.chunk.js"
+  },
+  {
+    "revision": "478bef5b7092d3b0df64",
+    "url": "/datta-able-react-admin/static/js/20.0593c749.chunk.js"
+  },
+  {
+    "revision": "6c18cacc4d9484896c25",
+    "url": "/datta-able-react-admin/static/js/21.d16e09d4.chunk.js"
+  },
+  {
+    "revision": "28ca7c4ba0684817085d",
+    "url": "/datta-able-react-admin/static/js/22.218c3918.chunk.js"
+  },
+  {
+    "revision": "d05752cf32742672632f",
+    "url": "/datta-able-react-admin/static/js/23.8c2e02d1.chunk.js"
+  },
+  {
+    "revision": "d2316d763b3c93c9856a",
+    "url": "/datta-able-react-admin/static/js/24.35e02131.chunk.js"
+  },
+  {
+    "revision": "886531654f687e010024",
+    "url": "/datta-able-react-admin/static/js/25.3bee0f5f.chunk.js"
+  },
+  {
+    "revision": "b4efaa40ecf9cf5d086a",
+    "url": "/datta-able-react-admin/static/js/5.07d844a0.chunk.js"
+  },
+  {
+    "revision": "ace6eeecead473a476cd2d73ae1583c6",
+    "url": "/datta-able-react-admin/static/js/5.07d844a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0521d9eeeb1eb6eaa1fd",
+    "url": "/datta-able-react-admin/static/js/6.50ec218a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/datta-able-react-admin/static/js/6.50ec218a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c251f18918cfdcce940",
+    "url": "/datta-able-react-admin/static/js/7.144f35fd.chunk.js"
+  },
+  {
+    "revision": "c46009a87f85d42f9c995bc337df230c",
+    "url": "/datta-able-react-admin/static/js/7.144f35fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac31503f423f06d5437",
+    "url": "/datta-able-react-admin/static/js/8.0086ed97.chunk.js"
+  },
+  {
+    "revision": "acf6783a0d506da63c39",
+    "url": "/datta-able-react-admin/static/js/9.dd8a831c.chunk.js"
+  },
+  {
+    "revision": "413185a73610cd6de277",
+    "url": "/datta-able-react-admin/static/js/main.a32ae7e0.chunk.js"
+  },
+  {
+    "revision": "368f3f01648b1f3e5feb",
+    "url": "/datta-able-react-admin/static/js/runtime-main.6dc09573.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/datta-able-react-admin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/datta-able-react-admin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/datta-able-react-admin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/datta-able-react-admin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/datta-able-react-admin/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
